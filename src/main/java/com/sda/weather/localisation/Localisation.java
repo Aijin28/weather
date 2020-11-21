f2680f8de9647f6818d9d31acad6635d53c71e2f
GM <m.glazowski06@gmail.com>
2020-11-21 12:12:17 +0100

    add localisation

---
+++ src/main/java/com/sda/weather/localisation/Localisation.java
@@ -0,0 +1,17 @@
+package com.sda.weather.localisation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Localisation {
+    Long id;
+    String cityName;
+    String countryName;
+    Float latitude;
+    Float longtitude;
+    String region;
+}
