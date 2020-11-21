09b29c11324cb0b38a41fc51f1ba6ca266a02a6f
Michal Paukszto <michal.paukszto@gmail.com>
2020-11-21 14:58:04 +0100

    refactor

--- src/test/java/com/sda/weather/LocalisationIntegrationTest.java
+++ src/test/java/com/sda/weather/LocalisationIntegrationTest.java
@@ -3,6 +3,7 @@ package com.sda.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -10,6 +11,6 @@ public class LocalisationIntegrationTest {
 
     @Test
     void test() {
-
+        assertThat(1).isEqualTo(1);
     }
 }
