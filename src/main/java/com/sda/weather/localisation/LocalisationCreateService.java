89d46dead13bd2f4d5141b08a2e487adde9af49f
GM <m.glazowski06@gmail.com>
2020-11-21 14:53:04 +0100

    LocalisationCreateService change

--- src/main/java/com/sda/weather/localisation/LocalisationCreateService.java
+++ src/main/java/com/sda/weather/localisation/LocalisationCreateService.java
@@ -10,15 +10,21 @@ public class LocalisationCreateService {
 
     final LocalisationRepository localisationRepository;
 
-    Localisation createLocalisation(String cityName, String countryName, String region, Float longitute, Float latitude) {
-        if (cityName == null && countryName == null && longitute == null && latitude == null) {
+    Localisation createLocalisation(String cityName, String countryName, String region, Float longitude, Float latitude) {
+        if (cityName == null && countryName == null && longitude == null && latitude == null) {
             throw new BadCreationParametersException("Parameters cannot be empty (region is optional)");
+        }
+        if (longitude < -90 && longitude > 90) {
+            throw new BadCreationParametersException("Longitude out of range (-90 to 90)");
+        }
+        if (latitude < -180 && latitude > 180) {
+            throw new BadCreationParametersException("Latitude out of range (-180 to 180)");
         }
         Localisation localisation = new Localisation();
         localisation.setCityName(cityName);
         localisation.setCountryName(countryName);
         localisation.setLatitude(latitude);
-        localisation.setLongitude(longitute);
+        localisation.setLongitude(longitude);
         if (region != null) {
             localisation.setRegion(region);
         }
