c6dd3a3850731589f51cda3685e016278eb83445
Michal Paukszto <michal.paukszto@gmail.com>
2020-11-21 12:21:29 +0100

    refactor

--- src/main/java/com/sda/weather/localisation/Localisation.java
+++ src/main/java/com/sda/weather/localisation/Localisation.java
@@ -12,6 +12,6 @@ public class Localisation {
     String cityName;
     String countryName;
     Float latitude;
-    Float longtitude;
+    Float longitude;
     String region;
 }
--- src/test/java/com/sda/weather/LocalisationIntegrationTest.java
+++ src/test/java/com/sda/weather/LocalisationIntegrationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WeatherApplicationTests {
+public class LocalisationIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void test() {
 
+    }
 }
